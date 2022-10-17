d225f1ebe68e28ccb879b7a0fd60f368d4cede5c
Ibnu Putra <azxarblog@gmail.com>
2022-10-17 20:19:21 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hallo</Text>
+        </View>
+    )
+}
+
+export default App
