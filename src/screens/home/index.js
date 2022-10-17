718287513923dc14821cd82111c0ac24ac374d7f
Ibnu Putra <azxarblog@gmail.com>
2022-10-17 22:48:59 +0700

    added vector icons and custom font

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from './styles';
 
 const HomeScreen = () => {
     return (
         <View>
-            <Text>HomeScreen</Text>
+            <Text style={styles.textCaption}>HomeScreen</Text>
+            <Icon name="rocket" size={30} color="#900" />
         </View>
     )
 }
---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import * as Colors from './colors'
+import * as Scaler from './scaler'
+import * as Typo from './typography'
+
+export { Colors, Scaler, Typo }
